9dbe687abc0ab3b3e64fda6a7f1c8043d202d907
Attila Kun <kun.attila.hun@gmail.com>
2025-04-07 21:58:53 +0200

    add polaris

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import '@shopify/polaris/build/esm/styles.css';
+import { AppProvider, Page, Card, TextField } from '@shopify/polaris'
+import translations from "@shopify/polaris/locales/en.json";
 
-function App() {
-  const [count, setCount] = useState(0)
+function AppPage() {
+  const [value, setValue] = useState('')
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Page title="Polaris Skeleton App">
+      <Card sectioned>
+        <TextField
+          label="Type something"
+          value={value}
+          onChange={setValue}
+          autoComplete="off"
+        />
+      </Card>
+    </Page>
+  )
+}
+
+function App() {
+  return (
+    <AppProvider i18n={translations}>
+      <AppPage />
+    </AppProvider>
   )
 }
 
