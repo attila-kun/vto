1d4ebe89f09a4f64c14faeccc6028967e745bb0d
Attila Kun <kun.attila.hun@gmail.com>
2025-04-12 17:58:55 +0200

    create shop table

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,16 @@
+-- with postgres user
 CREATE DATABASE vto;
 CREATE USER vto WITH PASSWORD 'vto';
 GRANT ALL PRIVILEGES ON DATABASE vto TO vto;
+
+\c vto
+
+GRANT ALL ON SCHEMA public TO vto;
+
+-- with vto user
+
+CREATE TABLE shop (
+    id BIGSERIAL PRIMARY KEY,
+    access_token TEXT NOT NULL,
+    shop_domain TEXT NOT NULL UNIQUE
+);
